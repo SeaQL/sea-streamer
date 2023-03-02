38bfbe8fd696ab227624f4b80b2e1c8b79bc1b5b
Chris Tsang <chris.2y3@outlook.com>
2023-03-02 23:57:39 +0800

    Ready to publish?

---
+++ build-tools/cargo-publish.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+cd sea-streamer-types
+cargo publish
+cd ..
+sleep 60
+
+cd sea-streamer-runtime
+cargo publish
+cd ..
+sleep 60
+
+cd sea-streamer-stdio
+cargo publish
+cd ..
+sleep 60
+
+cd sea-streamer-kafka
+cargo publish
+cd ..
+sleep 60
+
+cd sea-streamer-socket
+cargo publish
+cd ..
+sleep 60
+
+# publish `sea-streamer`
+cargo publish
