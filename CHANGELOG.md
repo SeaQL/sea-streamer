ff4cfdb9b09114461dd490e0677cc7910e057f04
Chris Tsang <chris.2y3@outlook.com>
2023-07-11 13:48:05 +0100

    Update build script

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,7 +5,7 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
-## 0.3.0 - Pending
+## 0.3.0 - 2023-07-11
 
 + Introducing `sea-streamer-file`: the File Backend
 + Added `File`, `OpenOptions`, `AsyncReadExt` etc to `sea-streamer-runtime`
--- build-tools/cargo-publish.sh
+++ build-tools/cargo-publish.sh
@@ -4,36 +4,33 @@ set -e
 cd sea-streamer-types
 cargo publish
 cd ..
-sleep 60
 
 cd sea-streamer-runtime
 cargo publish
 cd ..
-sleep 60
 
 cd sea-streamer-stdio
 cargo publish
 cd ..
-sleep 60
+
+cd sea-streamer-file
+cargo publish
+cd ..
 
 cd sea-streamer-kafka
 cargo publish
 cd ..
-sleep 60
 
 cd sea-streamer-redis
 cargo publish
 cd ..
-sleep 60
 
 cd sea-streamer-socket
 cargo publish
 cd ..
-sleep 60
 
 # publish `sea-streamer`
 cargo publish
-sleep 60
 
 cd examples
 cargo publish
